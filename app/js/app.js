5beb1e8dd1b3520daade3188f575eee92ab2183b
Evgeniy <260443@mail.ru>
2023-02-11 19:25:55 +0500

    Initial commit

---
+++ app/js/app.js
@@ -0,0 +1,59 @@
+import Swiper, { Navigation } from 'swiper';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Общие переменные
+  let body = document.querySelector('body');
+
+  // portfolio slider
+  const portfolioSlider = new Swiper('.portfolio-slider', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-next',
+    },
+  });
+
+  // Current year
+  let footerYear = document.querySelector('.footer .year');
+  let now = new Date();
+  let year = now.getFullYear();
+  footerYear.innerText = year;
+
+
+  // Мобильное меню
+  let burgerMenuWrapper = document.querySelector('.burger-menu-wrapper'),
+      mobileMenu = document.querySelector('.mobile-menu'),
+      burgerMenu = document.querySelector('.burger-menu');
+
+  burgerMenuWrapper.onclick = function () {
+    body.classList.toggle('overflow-hidden');
+    burgerMenuWrapper.classList.toggle('active');
+    mobileMenu.classList.toggle('mobile-menu-open');
+    burgerMenu.classList.toggle('close');
+  }
+
+  // закрытие меню при клике на parent элемент
+  let listParentClick = document.querySelectorAll('.mobile-menu a');
+  for (let i = 0; i < listParentClick.length; i++) {
+    listParentClick[i].onclick = function (event) {
+      event.preventDefault();
+      closeMenu();
+      let hrefClick = this.href;
+      setTimeout(function() {location.href = hrefClick}, 700);
+    }
+  }
+
+  function closeMenu () {
+    body.classList.remove('overflow-hidden');
+    burgerMenuWrapper.classList.remove('active');
+    mobileMenu.classList.remove('mobile-menu-open');
+    burgerMenu.classList.remove('close');
+  }
+
+
+  
+
+});
